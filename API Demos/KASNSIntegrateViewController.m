414306d033bae8d280a634007605ae82a1f566de
Evan JIANG <firstfan@gmail.com>
2014-07-11 08:14:23 +0900

    And link/unlink to SNS

--- API Demos/KASNSIntegrateViewController.m
+++ API Demos/KASNSIntegrateViewController.m
@@ -84,25 +84,35 @@
     andCallback :@selector(myRegistrationFinished:usingNetwork:withError:)];
 }
 
-- (void)myRegistrationFinished  :(KiiUser *)user
-        usingNetwork            :(KiiSocialNetworkName)network
-        withError               :(NSError *)error
+- (IBAction)linkToFacebook:(id)sender
 {
-    if (error == nil) {
-        [[KAGlobal getInstance] setToken:user.accessToken];
-        [[iToast makeText:@"Login successfully"] show];
-    } else {
-        [[iToast makeText:[NSString stringWithFormat:@"Login failed:%@", error]] show];
-        NSLog(@"Login failed:%@", error);
+    if ([KiiUser currentUser] == nil) {
+        [[iToast makeText:@"You need to login first."] show];
+        return;
     }
+
+    [KiiSocialConnect linkCurrentUserWithNetwork:kiiSCNFacebook
+    usingOptions                                :nil
+    withDelegate                                :self
+    andCallback                                 :@selector(myAccountLinkFinished:usingNetwork:withError:)];
 }
 
-- (IBAction)linkToFacebook:(id)sender {}
-
-- (IBAction)unlinkFromFacebook:(id)sender {}
-
-- (IBAction)loginWithTwitter:(id)sender
+- (IBAction)unlinkFromFacebook:(id)sender
 {
+    if ([KiiUser currentUser] == nil) {
+        [[iToast makeText:@"You need to login first."] show];
+        return;
+    }
+
+    [KiiSocialConnect unLinkCurrentUserWithNetwork:kiiSCNFacebook
+    withDelegate:self
+    andCallback :@selector(myAccountUnlinkFinished:usingNetwork:withError:)];
+}
+
+- (NSArray *)getTwitterAccounts
+{
+    __block NSArray *twitterAccounts = nil;
+
     if (NSClassFromString(@"ACAccountStore")) {
         ACAccountStore  *store = [[ACAccountStore alloc] init];
         ACAccountType   *twitterType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
@@ -116,33 +126,103 @@
                 ACAccountType *twitterTypeGranted = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
 
                 // Return an array of granted ACAccounts.
-                NSArray *twitterAccounts = [store accountsWithAccountType:twitterTypeGranted];
-                
-                if ([twitterAccounts count] == 0) {
-                    [[iToast makeText:@"There's no Twitter account existing now."] show];
-                    return;
-                }
-                /* INSERT KII CLOUD AUTHENTICATION LOGIC HERE */
-                ACAccount* account = twitterAccounts[0];
-                
-                // Fetch the OAuth token and secret.
-                NSDictionary *options = @{@"twitter_account": account};
-                
-                // Execute the login.
-                [KiiSocialConnect logIn:kiiSCNTwitter
-                           usingOptions:options
-                           withDelegate:self
-                            andCallback:@selector(myRegistrationFinished:usingNetwork:withError:)];
+                twitterAccounts = [store accountsWithAccountType:twitterTypeGranted];
             } else {
                 // handle error
                 [[iToast makeText:@"Cannot get Twitter accounts"] show];
             }
         }];
     }
+
+    return twitterAccounts;
 }
 
-- (IBAction)linkToTwitter:(id)sender {}
+- (IBAction)loginWithTwitter:(id)sender
+{
+    NSArray *twitterAccounts = [self getTwitterAccounts];
 
-- (IBAction)unlinkFromTwitter:(id)sender {}
+    if ((twitterAccounts != nil) && ([twitterAccounts count] == 0)) {
+        [[iToast makeText:@"There's no Twitter account existing now."] show];
+        return;
+    }
+
+    /* INSERT KII CLOUD AUTHENTICATION LOGIC HERE */
+    ACAccount *account = twitterAccounts[0];
+
+    // Fetch the OAuth token and secret.
+    NSDictionary *options = @{@"twitter_account": account};
+
+    // Execute the login.
+    [KiiSocialConnect logIn:kiiSCNTwitter
+    usingOptions:options
+    withDelegate:self
+    andCallback :@selector(myRegistrationFinished:usingNetwork:withError:)];
+}
+
+- (IBAction)linkToTwitter:(id)sender
+{
+    NSArray *twitterAccounts = [self getTwitterAccounts];
+
+    if ((twitterAccounts != nil) && ([twitterAccounts count] == 0)) {
+        [[iToast makeText:@"There's no Twitter account existing now."] show];
+        return;
+    }
+
+    ACAccount *account = twitterAccounts[0];
+
+    // define options parameter
+    NSDictionary *options = @{@"twitter_account": account};
+
+    [KiiSocialConnect linkCurrentUserWithNetwork:kiiSCNTwitter
+    usingOptions                                :options
+    withDelegate                                :self
+    andCallback                                 :@selector(myAccountLinkFinished:usingNetwork:withError:)];
+}
+
+- (IBAction)unlinkFromTwitter:(id)sender
+{
+    [KiiSocialConnect unLinkCurrentUserWithNetwork:kiiSCNTwitter
+    withDelegate:self
+    andCallback :@selector(myAccountUnlinkFinished:usingNetwork:withError:)];
+}
+
+- (void)myRegistrationFinished  :(KiiUser *)user
+        usingNetwork            :(KiiSocialNetworkName)network
+        withError               :(NSError *)error
+{
+    if (error == nil) {
+        [[KAGlobal getInstance] setToken:user.accessToken];
+        [[iToast makeText:@"Login successfully"] show];
+    } else {
+        [[iToast makeText:[NSString stringWithFormat:@"Login failed:%@", error]] show];
+        NSLog(@"Login failed:%@", error);
+    }
+}
+
+- (void)myAccountLinkFinished   :(KiiUser *)user
+        usingNetwork            :(KiiSocialNetworkName)network
+        withError               :(NSError *)error
+{
+    if (error == nil) {
+        // The user has linked successfully.
+        [[iToast makeText:@"Link successfully"] show];
+    } else {
+        [[iToast makeText:[NSString stringWithFormat:@"Link failed:%@", error]] show];
+        NSLog(@"Link failed:%@", error);
+    }
+}
+
+- (void)myAccountUnlinkFinished :(KiiUser *)user
+        usingNetwork            :(KiiSocialNetworkName)network
+        withError               :(NSError *)error
+{
+    if (error == nil) {
+        // The user has unlinked successfully
+        [[iToast makeText:@"Unlink successfully"] show];
+    } else {
+        [[iToast makeText:[NSString stringWithFormat:@"Unlink failed:%@", error]] show];
+        NSLog(@"Unlink failed:%@", error);
+    }
+}
 
 @end
